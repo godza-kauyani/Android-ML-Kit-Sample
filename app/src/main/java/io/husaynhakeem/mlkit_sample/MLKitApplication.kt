d1e3ed336b68a9590449c0fa5b7db6f8fa984d31
godza-kauyani <kauyanigodfrey@gmail.com>
2018-09-04 09:11:37 +0200

    Update MLKitApplication.kt
    Added appcenter

--- app/src/main/java/io/husaynhakeem/mlkit_sample/MLKitApplication.kt
+++ app/src/main/java/io/husaynhakeem/mlkit_sample/MLKitApplication.kt
@@ -1,6 +1,10 @@
 package io.husaynhakeem.mlkit_sample
 
+
 import android.app.Application
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics; 
+import com.microsoft.appcenter.crashes.Crashes;
 
 
 class MLKitApplication: Application() {
@@ -8,6 +12,7 @@ class MLKitApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        AppCenter.start(getApplication(), "bb251a9e-8fd5-452e-8cb2-91787ba0f4a2", Analytics.class, Crashes.class);
     }
 
     companion object {
